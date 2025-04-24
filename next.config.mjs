c5d31154764fe1928b80d6cfdbb510d2c18d1b0a
dvp-tae <th3016@naver.com>
2025-04-24 12:01:55 +0900

    feat:PWA 세팅

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWAFactory from "next-pwa";
 
-export default nextConfig;
+const withPwa = withPWAFactory({
+  dest: "public",
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
+
+export default withPwa(nextConfig);
