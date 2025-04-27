7bdb6352c7a914e16f4df89072875931a8fa909b
youth-o <127082479+youth-o@users.noreply.github.com>
2025-04-28 00:37:57 +0900

    feat: input 컴포넌트 제작

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,22 @@
 import { Container } from "@/components";
-import { Button } from "@/components/ui/button";
+import { Input } from "@/components/common/input/input";
 
 export default function Home() {
+  const passwordChecks = [
+    { label: "영문 포함", isChecked: true },
+    { label: "숫자 포함", isChecked: false },
+    { label: "8-20자 이내", isChecked: true },
+  ];
+
   return (
     <Container className="bg-center bg-cover bg-main">
       <div className="flex flex-col gap-10">
         <div className="flex flex-col gap-2">
           <h1 className="text-h1">Zaply</h1>
           <p className="text-b1R">Create once. Spread with Zaply</p>
-          <Button variant="active">button</Button>
-          <Button variant="deactive">button</Button>
-          <Button variant="subAction">button</Button>
-          <Button variant="active" size="sm">
-            button
-          </Button>
-          <Button variant="deactive" size="sm">
-            button
-          </Button>
-          <Button variant="subAction" size="sm">
-            button
-          </Button>
+          <Input placeholder="하이" type="check" className="w-[200px]" />
+          <Input type="check" placeholder="비밀번호를 입력하세요" checkItems={passwordChecks} />
+          <Input type="timer" placeholder="타이머" />
         </div>
       </div>
     </Container>
