8a5016db6a2533c5e57920c82f9317fdd40dcca3
dvp-tae <th3016@naver.com>
2025-04-27 17:04:41 +0900

    feat: 아이콘 디자인 시스템 등록

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,138 @@
 import { Container } from "@/components";
+import {
+  ChevronIcon,
+  ArrowIcon,
+  RefreshIcon,
+  DownLoadIcon,
+  UpLoadIcon,
+  SwapIcon,
+  HamburgerIcon,
+  ListIcon,
+  EllipsisIcon,
+  EditIcon,
+  ChartIcon,
+  ChatDotIcon,
+  UserIcon,
+  ActivityIcon,
+  EmailIcon,
+  PasswordIcon,
+  HeartIcon,
+  SettingsIcon,
+  BellIcon,
+  TrashIcon,
+  DeleteIcon,
+  StickerIcon,
+  ImageIcon,
+  LinkIcon,
+  ShareIcon,
+  LikeIcon,
+  HateIcon,
+  StarIcon,
+  FlagIcon,
+  SearchIcon,
+  CameraIcon,
+  LightHubIcon,
+  TVIcon,
+  FilmIcon,
+  VideoIcon,
+  SaveIcon,
+  TagIcon,
+  BookMarkIcon,
+  FileIcon,
+  CancelIcon,
+  DangerIcon,
+  ErrorIcon,
+  HelpIcon,
+  InfoIcon,
+  CircleCheckIcon,
+  CheckIcon,
+  CircleIcon,
+  XIcon,
+  RemoveIcon,
+  AddIcon,
+  CircleCheckBoldIcon,
+} from "@/components/icons";
 
 export default function Home() {
   return (
-    <Container className="bg-center bg-cover bg-main">
-      <div className="flex flex-col gap-10">
-        <div className="flex flex-col gap-2">
-          <h1 className="text-h1">Zaply</h1>
-          <p className="text-b1R">Create once. Spread with Zaply</p>
-        </div>
+    <Container className="flex flex-col space-y-4 bg-grayscale-500">
+      <div className="flex space-x-2">
+        <ChevronIcon type="right" />
+        <ChevronIcon type="left" />
+        <ChevronIcon type="up" />
+        <ChevronIcon type="down" />
+        <ArrowIcon type="right" />
+        <ArrowIcon type="left" />
+        <ArrowIcon type="up" />
+        <ArrowIcon type="down" />
+      </div>
+
+      <div className="flex space-x-2">
+        <RefreshIcon />
+        <DownLoadIcon />
+        <UpLoadIcon />
+        <SwapIcon />
+        <HamburgerIcon />
+        <ListIcon />
+        <EllipsisIcon type="horizontal" />
+        <EllipsisIcon type="vertical" />
+      </div>
+
+      <div className="flex space-x-2">
+        <EditIcon />
+        <ChartIcon />
+        <ChatDotIcon />
+        <UserIcon />
+        <ActivityIcon />
+        <EmailIcon />
+        <PasswordIcon />
+        <HeartIcon />
+      </div>
+
+      <div className="flex space-x-2">
+        <SettingsIcon />
+        <BellIcon />
+        <TrashIcon />
+        <DeleteIcon />
+        <StickerIcon />
+        <ImageIcon />
+        <LinkIcon />
+        <ShareIcon />
+      </div>
+
+      <div className="flex space-x-2">
+        <LikeIcon />
+        <HateIcon />
+        <StarIcon />
+        <FlagIcon />
+        <SearchIcon />
+        <CameraIcon />
+        <LightHubIcon />
+        <TVIcon />
+      </div>
+
+      <div className="flex space-x-2">
+        <FilmIcon />
+        <VideoIcon />
+        <SaveIcon />
+        <TagIcon />
+        <BookMarkIcon />
+        <FileIcon />
+      </div>
+
+      <div className="flex space-x-2">
+        <CancelIcon />
+        <DangerIcon />
+        <ErrorIcon />
+        <HelpIcon />
+        <InfoIcon />
+        <CircleCheckIcon />
+        <CheckIcon />
+        <CircleIcon />
+        <XIcon />
+        <RemoveIcon />
+        <AddIcon />
+        <CircleCheckBoldIcon />
       </div>
     </Container>
   );
