ee8d4e2f2535d40a0430973f9b6a77344c7ce648
youth-o <127082479+youth-o@users.noreply.github.com>
2025-04-27 17:27:49 +0900

    feat: 버튼 컴포넌트 제작

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Container } from "@/components";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -7,6 +8,18 @@ export default function Home() {
         <div className="flex flex-col gap-2">
           <h1 className="text-h1">Zaply</h1>
           <p className="text-b1R">Create once. Spread with Zaply</p>
+          <Button variant="active">button</Button>
+          <Button variant="deactive">button</Button>
+          <Button variant="subAction">button</Button>
+          <Button variant="active" size="sm">
+            button
+          </Button>
+          <Button variant="deactive" size="sm">
+            button
+          </Button>
+          <Button variant="subAction" size="sm">
+            button
+          </Button>
         </div>
       </div>
     </Container>
---
+++ src/components/ui/button.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const buttonVariants = cva("h-[50px] flex items-center justify-center gap-2 text-b1M", {
+  variants: {
+    variant: {
+      active: "bg-blue-blueblack text-grayscale-100",
+      deactive: "bg-grayscale-400 text-grayscale-100",
+      subAction: "bg-grayscale-100 text-blue-700 border-grayscale-300",
+    },
+    size: {
+      default: "w-full py-3 px-2 rounded-full",
+      sm: "w-[120px] py-3 px-4 rounded-[12px]",
+    },
+  },
+  defaultVariants: {
+    variant: "active",
+    size: "default",
+  },
+});
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, leftIcon, rightIcon, children, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
+    return (
+      <Comp className={cn(buttonVariants({ variant, size }), className)} ref={ref} {...props}>
+        {leftIcon && <span className="flex items-center">{leftIcon}</span>}
+        {children}
+        {rightIcon && <span className="flex items-center">{rightIcon}</span>}
+      </Comp>
+    );
+  }
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
