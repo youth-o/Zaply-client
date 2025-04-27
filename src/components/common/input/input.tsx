3e4d9fb117904ac7a1ed1d74656834d55a1a07e2
youth-o <127082479+youth-o@users.noreply.github.com>
2025-04-28 03:04:21 +0900

    design: input CSS 수정

--- src/components/common/input/input.tsx
+++ src/components/common/input/input.tsx
@@ -13,47 +13,69 @@ const Input = React.forwardRef<
     type?: "text" | "check" | "timer";
     timerText?: string;
     checkItems?: CheckItem[];
+    inputSize?: "default" | "md" | "sm";
   }
->(({ width, className, type = "text", placeholder, timerText, checkItems = [], ...props }, ref) => {
-  return (
-    <div className="relative w-full">
-      <div className={cn("relative w-full", width)}>
-        <input
-          type={type === "check" ? "text" : type}
-          className={cn(
-            "flex h-[50px] w-full px-5 py-[14px] outline-none bg-grayscale-100 border border-grayscale-300 rounded-[12px] text-[14px] font-medium text-grayscale-800 placeholder:text-grayscale-600 placeholder:font-normal placeholder:text-b3R focus:border-grayscale-700",
-            className
+>(
+  (
+    {
+      width,
+      inputSize = "default",
+      className,
+      type = "text",
+      placeholder,
+      timerText,
+      checkItems = [],
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <div
+        className={cn(
+          "relative",
+          inputSize === "default" && "w-full",
+          inputSize === "md" && "w-[220px]",
+          inputSize === "sm" && "w-[161px]",
+          width
+        )}>
+        <div className="relative w-full">
+          <input
+            type={type === "check" ? "text" : type}
+            className={cn(
+              "flex h-[50px] w-full px-5 py-[14px] outline-none bg-grayscale-100 border border-grayscale-300 rounded-[12px] text-[14px] font-medium text-grayscale-800 placeholder:text-grayscale-600 placeholder:font-normal placeholder:text-b3R focus:border-grayscale-700",
+              className
+            )}
+            placeholder={placeholder}
+            ref={ref}
+            {...props}
+          />
+          {type === "timer" && (
+            <div className="absolute right-5 top-1/2 -translate-y-1/2 text-b3M text-redscale-700">
+              {timerText ?? "03:00"}
+            </div>
           )}
-          placeholder={placeholder}
-          ref={ref}
-          {...props}
-        />
-        {type === "timer" && (
-          <div className="absolute right-5 top-1/2 -translate-y-1/2 text-b3M text-redscale-700">
-            {timerText ?? "03:00"}
+        </div>
+
+        {type === "check" && checkItems.length > 0 && (
+          <div className="flex flex-wrap gap-4 items-center mt-2 pl-3">
+            {checkItems.map((item, index) => (
+              <div key={index} className="flex gap-1 items-center">
+                <CheckIcon stroke={item.isChecked ? "#22C7FA" : "#BDC5D0"} />
+                <p
+                  className={cn(
+                    "!text-b4R",
+                    item.isChecked ? "text-blue-700" : "text-grayscale-500"
+                  )}>
+                  {item.label}
+                </p>
+              </div>
+            ))}
           </div>
         )}
       </div>
-
-      {type === "check" && checkItems.length > 0 && (
-        <div className="flex flex-wrap gap-4 items-center mt-2 pl-3">
-          {checkItems.map((item, index) => (
-            <div key={index} className="flex gap-1 items-center">
-              <CheckIcon stroke={item.isChecked ? "#22C7FA" : "#BDC5D0"} />
-              <p
-                className={cn(
-                  "!text-b4R",
-                  item.isChecked ? "text-blue-700" : "text-grayscale-500"
-                )}>
-                {item.label}
-              </p>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
-});
+    );
+  }
+);
 Input.displayName = "Input";
 
 export { Input };
