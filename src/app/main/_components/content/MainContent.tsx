5a538a986cf1834d5920162aad6bf0821472ca7e
youth-o <127082479+youth-o@users.noreply.github.com>
2025-05-03 21:30:10 +0900

    feat: 토글 버튼 구현

--- src/app/main/_components/content/MainContent.tsx
+++ src/app/main/_components/content/MainContent.tsx
@@ -1,5 +1,11 @@
+import ContentToggle from "./ContentToggle";
+
 const MainContent = () => {
-  return <main className="flex flex-col gap-4"></main>;
+  return (
+    <main className="flex flex-col gap-4">
+      <ContentToggle />
+    </main>
+  );
 };
 
 export default MainContent;
